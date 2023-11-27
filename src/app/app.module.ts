f5390f553b52e4593308be8acc9a13cdb0ae34d1
Alisson Rego <alissondroid07@gmail.com>
2023-11-27 15:51:32 -0300

    Criação do layout de fundo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   declarations: [
     AppComponent,
     TelaVendaComponent,
-    SidebarComponent
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
