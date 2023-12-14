01d3a64367c57d3761fabee6ff8e853dc634b6df
Alisson Rego <alissondroid07@gmail.com>
2023-12-14 15:10:57 -0300

    Criadas as validações do formulário

--- src/app/pages/tela-produtos/cadastrar-produtos/cadastrar-produtos.component.ts
+++ src/app/pages/tela-produtos/cadastrar-produtos/cadastrar-produtos.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-produtos',
   templateUrl: './cadastrar-produtos.component.html',
   styleUrls: []
 })
-export class CadastrarProdutosComponent {
+export class CadastrarProdutosComponent implements OnInit {
+  
+  ngOnInit(): void {  }
+
+  categoria = ['Cereais', 'Bebidas', 'Carnes'];
+
+  produtoForm = new FormGroup({
+    titulo: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    categoriaSelect: new FormControl('', Validators.required),
+    descricao: new FormControl('', Validators.maxLength(100)),
+    valorUnitario: new FormControl('', [Validators.required, Validators.min(0.01)])
+  })
+
+  get titulo() {
+    return this.produtoForm.get('titulo');
+  }
+
+  get categoriaSelect() {
+    return this.produtoForm.get('categoriaSelect');
+  }
+
+  get descricao() {
+    return this.produtoForm.get('descricao');
+  }
+
+  get valorUnitario() {
+    return this.produtoForm.get('valorUnitario');
+  }
+
   openPopUpIncluir: boolean = false;
 
   abrirDialogoIncluir() {
@@ -16,13 +44,13 @@ export class CadastrarProdutosComponent {
   fecharDialogoIncluir() {
     this.openPopUpIncluir = false;
   }
-  
-  categoria = ['Cereais', 'Bebidas', 'Carnes'];
 
-  produtoForm = new FormGroup({
-    titulo: new FormControl(''),
-    categoriaSelect: new FormControl(''),
-    valorUnitario: new FormControl('')
-  })
+  resetForm() {
+    this.produtoForm.reset();
+  }
+
+  onSubmit() {
+    this.produtoForm.valid ? this.abrirDialogoIncluir() : alert("Todos os campos precisam estar preenchidos");
+  }
 
 }
